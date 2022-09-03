476add8f46ddee7bb7da5b4d313c1095bc222b2b
ShafiaChy <shafiachy04@gmail.com>
2022-09-03 15:41:51 +0600

    errors added

--- js/utilities.js
+++ js/utilities.js
@@ -2,18 +2,18 @@ function getInputFieldValueById(inputFieldId) {
   const inputField = document.getElementById(inputFieldId);
   const inputFieldValueString = inputField.value;
   const inputFieldValue = parseFloat(inputFieldValueString);
-  inputField.value = "";
   return inputFieldValue;
+  inputField.value = "";
 }
 
 function getTextElementValueById(elementId) {
   const textElement = document.getElementById(elementId);
   const textElementValueString = textElement.innerText;
-  const textElementValue = parseFloat(textElementValueString);
-  return textElementValue;
+  const textElementValue = textElementValueString;
+  textElementValue;
 }
 
 function setTextElementValueById(elementId, newValue) {
   const textElement = document.getElementById(elementId);
-  textElement.innerText = newValue;
+  textElement.value = newValue;
 }
